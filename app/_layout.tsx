c5e034c968b4902bc8debc9ff0a19124222402bb
Belend98 <161621843+Belend98@users.noreply.github.com>
2026-05-27 10:10:26 +0200

    Add : safe area view

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,16 @@
 import { Stack } from 'expo-router'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="(tabs)" />
-      <Stack.Screen name="(auth)" />
-    </Stack>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }} edges={['top', 'left', 'right']}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="(auth)" />
+        </Stack>
+      </SafeAreaView>
+    </SafeAreaProvider>
   )
 }
